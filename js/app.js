67b72f1add9f4e31b6e5e10a942547f2c65bf78d
simretB05 <happysimret@gmail.com>
2023-03-21 20:10:56 -0600

    check for admin in a function

--- js/app.js
+++ js/app.js
@@ -6,4 +6,22 @@ function generic_greeting(user)
 generic_greeting( `buba` )
 generic_greeting( `simret` )
 generic_greeting( `chaki su` )
-generic_greeting(`elmo`)
+generic_greeting( `elmo` )
+
+
+
+function find_specific_greeting( users )
+{
+    for ( let counter = 0; counter < users.length; counter++ )
+    {
+        if ( users === `admin` )
+        {
+
+            return true
+        }
+    }
+    return false
+
+}
+let check_for_admin = find_specific_greeting( `simret` )
+console.log(check_for_admin)
